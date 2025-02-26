77149a43a2dc24aeaa979335d6870dec1fd6efe7
Martin Jul <martin@mjul.com>
2025-02-26 23:47:58 +0100

    Add a call to the Gemini API

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-def main():
-    print("Hello from audio-transcription!")
+import os
+from google import genai
 
+def main():
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if not api_key:
+        raise ValueError("GEMINI_API_KEY environment variable not set.")
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-exp", contents="How does RLHF work?"
+    )
+    print(response.text)
 
 if __name__ == "__main__":
     main()
